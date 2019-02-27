b3de6b6c4d8c193ebb8fe3ba297995d911df5abb
AJ Guiles <ashiguiles@gmail.com>
2019-02-27 04:23:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,34 +1,19 @@
-# def prime?(int)
-#   if int == 1 
-#     return true 
-#     elsif int < 1 
-#     return false 
-#   else 
-#     for x in 2..(int-1)
-#     if (int % x)== 0 
-#       return false
-#     end
-#   end
-# end
+def prime?(int)
+  if int == 1 
+    return true 
+    elsif int < 1 
+    return false 
+  else 
+    for x in 2..(int-1)
+    if (int % x)== 0 
+      return false
+    end
+  end
+end
 
-# true
-# end
+true
+end
 
   
 
-def prime?(x)
-  if x == 1
-    return true 
-  elsif x < 1
-    return false
-  else
-  for y in 2..(x-1)
-    if (x % y) == 0
-      return false 
-    end
-  end 
-end
-
-  true
-end
 
