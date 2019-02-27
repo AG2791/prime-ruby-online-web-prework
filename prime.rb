6a6b83581aa76d451dc88a51c8d512acd29502df
AJ Guiles <ashiguiles@gmail.com>
2019-02-27 04:22:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,18 @@
-def prime?(int)
-  if int == 1 
-    return true 
-    elsif int < 1 
-    return false 
-  else 
-    for x in 2..(int-1)
-    if (int % x)== 0 
-      return false
-    end
-  end
-end
+# def prime?(int)
+#   if int == 1 
+#     return true 
+#     elsif int < 1 
+#     return false 
+#   else 
+#     for x in 2..(int-1)
+#     if (int % x)== 0 
+#       return false
+#     end
+#   end
+# end
 
-true
-end
+# true
+# end
 
   
 
